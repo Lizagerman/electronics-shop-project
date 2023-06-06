fc9cf289def7b938a8c86bf493c2a11dfc439971
Лиза Герман <liza36046@gmail.com>
2023-06-06 20:38:07 +0300

    )

--- homework-1/item.py
+++ homework-1/item.py
@@ -1,32 +1,92 @@
-class Item:
-    pass
+import csv
+import os
+from abc import ABC
 
-if __name__ == '__main__':
-    item1 = Item("Смартфон", 10000, 20)
-    item2 = Item("Ноутбук", 20000, 5)
-
-    item1.name = "Смартфон"
-    item1.price = 10000
-    item1.quantity = 20
+path = os.path.join('..', 'src', 'items.csv')  # путь к файлу
 
 
-    item2.name = "Ноутбук"
-    item2.price = 20000
-    item2.quantity = 5
+class InstantiateCSVError(Exception):
+
+    def __init__(self, message, base_message=None):
+        self.base_message = base_message
+        self.message = message
+
+    def __str__(self):
+        if self.base_message is None:
+            return self.message
+
+        return f'{self.message} - {str(self.base_message)}'
 
 
+class Item(ABC):
 
-    print(item1.calculate_total_price(item1.price*item1.quantity))
-    print(item2.calculate_total_price(item2.price*item2.quantity))
+    pay_rate = 1.
+    all = []
 
-    # устанавливаем новый уровень цен
-    Item.pay_rate = 0.8
-    # применяем скидку
-    item1.apply_discount(item1.price * Item.pay_rate)
-    item2.apply_discount(item2.price * Item.pay_rate)
+    # path = os.path.join('..', 'electronics-shop-project_AV', 'scr', 'items.csv')
 
-    print(item1.price)
-    print(item2.price)
+    def __init__(self, name: str, price: float, quantity: int) -> None:
 
-    print(Item.all)
+        self.verify_name(name)
+
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        super().__init__()
+
+    @classmethod
+    def verify_name(cls, name):
+        if len(name) >= 15:
+            raise Exception("Длина наименования товара превышает 10 символов.")
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all = []
+        try:
+            with open(path, encoding='windows-1251', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                cls.all = [cls((row['name']), float(row['price']), int(row['quantity'])) for row in reader]
+
+        except FileNotFoundError:
+            print('_Отсутствует файл item.csv_')
+
+        except KeyError as e:
+            print(InstantiateCSVError("_Файл item.csv поврежден_", e))
+        print(cls.all)
+        return cls.all
+
+    @staticmethod
+    def string_to_number(str_number):
+        return int(float(str_number))
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        self.verify_name(name)
+        self.__name = name
+
+    def calculate_total_price(self) -> float:
+
+        return self.price * self.quantity * self.pay_rate
+
+    def apply_discount(self) -> float:
+
+        self.price *= self.pay_rate
+        return self.pay_rate
+
+    def __str__(self):
+        return str(self.name)
+
+    def __repr__(self):
+        return f"{__class__.__name__}('{str(self.__name)}', {str(self.price)}, {str(self.quantity)})"
+
+    def __add__(self, other):
+        temp = other.__class__.__name__  # имя другого класса (str)
+        if issubclass(other.__class__, self.__class__):
+            return self.quantity + other.quantity
+        else:
+            return print("Складывать нельзя")
 
